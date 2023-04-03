91336b800a2231453b52973dd047cfc835a744b8
r-marmor <raphael.marmor@gmail.com>
2023-04-03 22:44:07 +0200

    added calculator and caesarCipher

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 export default function capitalize(str) {
-    let result = "";
-    let newStr = str.replace(/[^a-zA-Z\s]/g, '');
-    
-    const firstLetter = newStr[0];
-    result += firstLetter.toUpperCase();
-
-    for (let i = 1; i < newStr.length; i++) {
-        result += newStr[i].toLowerCase();
-    }
-    return result;
+    const newStr = str.replace(/[^a-zA-Z\s]/g, '');
+    const firstLetter = newStr[0].toUpperCase();
+    const restOfStr = newStr.slice(1).toLowerCase();
+    return `${firstLetter}${restOfStr}`;
 }
 
 export function reverseString(str) {
@@ -29,3 +23,33 @@ export const calculator = {
         return x * y;
     }
 };
+
+export function caesarCipher(plaintext, shift) {
+    // check for edge cases
+    while (shift > 26) {
+        shift -= 26;
+    }
+    // handle edge case negative shift
+    if (shift < 0) shift = 26 + shift;
+    // create an empty string to store the output
+    let result = "";
+    // Loop through each character in the plaintext
+    for (let i = 0; i < plaintext.length; i++) {
+        let asciiCode = plaintext.charCodeAt(i);
+        
+        // Check if character is uppercase
+        if (asciiCode >= 65 && asciiCode <= 90) {
+            asciiCode = ((asciiCode - 65 + shift) % 26) + 65;
+        }
+        // Check if character is lowercase
+        else if (asciiCode >= 97 && asciiCode <= 122) {
+            asciiCode = ((asciiCode - 97 + shift) % 26) + 97;
+        } else {
+            result += plaintext[i];
+            continue;
+        }
+        // Convert ascii code back to character and add it to the result;
+        result += String.fromCharCode(asciiCode);
+    }
+    return result; 
+}
