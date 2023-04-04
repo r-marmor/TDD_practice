8a8faa9f393f67b72f0282c744d2010d352b479e
r-marmor <raphael.marmor@gmail.com>
2023-04-04 10:22:11 +0200

    finished implementing all functions and tests

--- script.js
+++ script.js
@@ -53,3 +53,15 @@ export function caesarCipher(plaintext, shift) {
     }
     return result; 
 }
+
+export function analyzeArray(arr) {
+    if (!Array.isArray(arr) || arr.length == 0) {
+        throw new Error('Invalid input');
+    }
+    
+    const average = arr.reduce((prev, curr) => prev + curr, 0)/arr.length;
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const length = arr.length;
+    return { average, min, max, length };
+}
--- script.test.js
+++ script.test.js
@@ -1,43 +1,48 @@
-import capitalize, { caesarCipher } from "./script";
-import { reverseString } from "./script";
-import { calculator } from "./script";
+import capitalize, { reverseString, calculator, caesarCipher, analyzeArray } from "./script";
 
-test('"random string" should become "Random string"', () => {
-        expect(capitalize("random string")).toBe("Random string");
+
+describe('capitalize()', () => {
+        test('"random string" should become "Random string"', () => {
+                expect(capitalize("random string")).toBe("Random string");
+        });
+
+        test('"RANDOM STRING" shoud become "Random string', () => {
+                expect(capitalize("RANDOM STRING")).toBe("Random string");
+        });
+
+        test('"@123random string" should become "Random string', () => {
+                expect(capitalize("@123random string")).toBe("Random string");
+        });
 });
 
-test('"RANDOM STRING" shoud become "Random string', () => {
-        expect(capitalize("RANDOM STRING")).toBe("Random string");
+describe('reverseString()', () => {
+        test("The function should reverse the string", () => {
+                expect(reverseString("reverse string")).toBe("gnirts esrever");
+        });
 });
 
-test('"@123random string" should become "Random string', () => {
-        expect(capitalize("@123random string")).toBe("Random string");
+describe('calculator()', () => {
+        test('The method add should sum the two numbers put as parameters', () => {
+                expect(calculator.add(1, 2)).toBe(3);
+        });
+
+        test('The method substract should substract the two numbers put as parameters', () => {
+                expect(calculator.substract(2, 1)).toBe(1);
+                expect(calculator.substract(1, 4)).toBe(-3);
+        });
+
+        test('The method divide should divide the two numbers put as parameters', () => {
+                expect(calculator.divide(4, 2)).toBe(2);
+                expect(calculator.divide(2, 4)).toBe(0.5);
+        });
+
+        test('The method multitply should multiply the two numbers put as parameters', () => {
+                expect(calculator.multiply(4, 2)).toBe(8);
+                expect(calculator.multiply(100, 0)).toBe(0);
+        });
 });
 
-test("The function should reverse the string", () => {
-        expect(reverseString("reverse string")).toBe("gnirts esrever");
-});
-
-test('The method add should sum the two numbers put as parameters', () => {
-        expect(calculator.add(1, 2)).toBe(3);
-});
-
-test('The method substract should substract the two numbers put as parameters', () => {
-        expect(calculator.substract(2, 1)).toBe(1);
-        expect(calculator.substract(1, 4)).toBe(-3);
-});
-
-test('The method divide should divide the two numbers put as parameters', () => {
-        expect(calculator.divide(4, 2)).toBe(2);
-        expect(calculator.divide(2, 4)).toBe(0.5);
-});
-
-test('The method multitply should multiply the two numbers put as parameters', () => {
-        expect(calculator.multiply(4, 2)).toBe(8);
-        expect(calculator.multiply(100, 0)).toBe(0);
-});
-
-describe('caesarCipher', () => {
+describe('caesarCipher()', () => {
         test('should shift letters by the given amount', () => {
             expect(caesarCipher('abc', 1)).toBe('bcd');
             expect(caesarCipher('xyz', 3)).toBe('abc');
@@ -66,3 +71,20 @@ describe('caesarCipher', () => {
             expect(caesarCipher('  ', 1)).toBe('  ');
         });
     });
+
+describe('analyzeArray()', () => {
+        test('throws an error if the input is not a non-empty array', () => {
+                expect(() => analyzeArray([])).toThrow("Invalid input");
+                expect(() => analyzeArray('not an array')).toThrow('Invalid input');
+        });
+        test('takes an array of numbers and returns an object with average, min, max, and length', () => {
+                const arr = [1,8,3,4,2,6];
+                expect(analyzeArray(arr)).toStrictEqual({ average: 4, min: 1, max: 8, length: 6 });
+                
+        });
+        test('returns the correct values for an array with negative numbers', () => {
+                const arr = [-2, 0, 3, -5, 1];
+                expect(analyzeArray(arr)).toStrictEqual({ average: -0.6, min: -5, max: 3, length: 5 });
+              });
+
+});
